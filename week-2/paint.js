ec5613ed10a4eb4be333c4f97098ca0ce1f0998c
Max Kohler <max@koehler-kn.de>
2019-03-17 16:59:18 +0000

    Build out type paint interface

--- week-2/paint.js
+++ week-2/paint.js
@@ -8,8 +8,9 @@ let brush = {
     color: 'rgb(250, 252, 251)',
     stroke: 'rgb(5, 2, 1)',
     angle: -45,
-    text: 'Digital Aesthetics',
-    fontFamily: 'Arial'
+    text: 'Good Times',
+    fontFamily: 'Arial',
+    rotate: false
 }
 
 function updateBrush() {
@@ -33,7 +34,9 @@ function handleMouseMove(pos) {
     if (mouseDown) {
         c.save()
         updateBrush();
-        brush.angle += 1;
+        if (brush.rotate === 'true') {
+            brush.angle += 1;
+        }
         let x = pos.x - (brush.size / 10);
         let y = pos.y - (brush.size / 2);
         c.translate(x, y)
@@ -45,8 +48,54 @@ function handleMouseMove(pos) {
 }
 
 function bindEvents() {
+    let textEl = document.querySelector('#brush-text')
+    let fillEl = document.querySelector('#brush-fill')
+    let strokeEl = document.querySelector('#brush-stroke')
+    let sizeEl = document.querySelector('#brush-size')
+    let angleEl = document.querySelector('#brush-angle')
+    let rotateEl = document.querySelectorAll('#brush-rotate input')
+    let dropDownEl = document.querySelectorAll('#brush-rotate input')
 
+    textEl.addEventListener('change', function (e) {
+        brush.text = e.srcElement.value;
+    })
+    fillEl.addEventListener('change', function (e) {
+        brush.color = e.srcElement.value;
+    })
+    strokeEl.addEventListener('change', function (e) {
+        brush.stroke = e.srcElement.value;
+    })
+    sizeEl.addEventListener('change', function (e) {
+        brush.size = e.srcElement.value;
+    })
+    angleEl.addEventListener('change', function (e) {
+        brush.angle = e.srcElement.value;
+    })
+    if (dropDownEl) {
+        let summaryEl = document.querySelector('#pickTypeface summary')
+        let detailsEl = document.querySelector('#pickTypeface details')
+        dropDownItems = document.querySelectorAll('#pickTypeface .dropdown-item')
 
+        for (let i = 0; i < dropDownItems.length; i++) {
+            let li = dropDownItems[i];
+            li.addEventListener('click', function (e) {
+                for (let i = 0; i < dropDownItems.length; i++) {
+                    dropDownItems[i].classList.remove('active')
+                }
+                summaryEl.innerHTML = e.srcElement.innerText;
+                brush.fontFamily = e.srcElement.innerText;
+                detailsEl.removeAttribute('open')
+            })
+        }
+    }
+    console.log(rotateEl)
+    for (let i = 0; i < rotateEl.length; i++) {
+        console.log(rotateEl)
+        rotateEl[i].addEventListener('click', function (e) {
+            brush.rotate = e.srcElement.value;
+        })
+
+    }
     c.canvas.addEventListener('mousedown', function (e) {
         mouseDown = true;
     });
