283ac2f54df17ca795373f131dcbb9af38fb0f15
Max Kohler <max@koehler-kn.de>
2019-04-18 17:40:58 +0100

    Add showpiece

--- week-2/app.js
+++ week-2/app.js
@@ -1,8 +1,8 @@
 let copy = '';
 let copyEl;
 //const source = 'gutenberg'
-const baseURL = 'http://avh-sammlung.de/max'
-//const baseURL = '.'
+//const baseURL = 'http://avh-sammlung.de/max'
+const baseURL = '.'
 
 class Source {
     constructor(id, bookTitle, year, author) {
@@ -176,7 +176,6 @@ window.addEventListener('DOMContentLoaded', function () {
     init()
 
     sources.push(new Source('gutenberg', 'The 36-Line Bible', 1495, 'Johannes Gutenberg'))
-    sources.push(new Source('gutenberg-2', 'The 36-Line Bible', 1495, 'Johannes Gutenberg'))
     sources.push(new Source('benjamin', 'The work of art in the age of mechanical reproduction', 1935, 'Walter Benjamin'))
     sources.push(new Source('nyt', 'The New York Times', 2019, ''))
     sources.push(new Source('darwin', 'On the Origin of Species', 1857, 'Charles Darwin'))
--- week-2/processImage.bat
+++ week-2/processImage.bat
@@ -1,5 +1,5 @@
 
-for %%i in (./scans/%1/*.jpg) do (tesseract ./scans/%1/"%%i" "./scans/%1/partial-text/%%i")
+for %%i in (./scans/%1/*.jpg) do (tesseract ./scans/%1/"%%i" "./scans/%1/partial-text/%%i" -l lat) 
 node mergePartialText.js -s %1
-for %%i in (./scans/%1/*.jpg) do (tesseract ./scans/%1/"%%i" "./scans/%1/ocr-%%i" tsv)
+for %%i in (./scans/%1/*.jpg) do (tesseract ./scans/%1/"%%i" "./scans/%1/ocr-%%i" tsv -l lat)
 node makeWordImages -s %1
